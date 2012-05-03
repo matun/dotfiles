d8d625acdf5c081b849c199fe8e71c84c8695ea8
matsumoto <matsumoto@localhost.localdomain>
2012-05-03 21:57:11 +0900

    First commit

---
+++ .emacs.d/init.el
@@ -0,0 +1,11 @@
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory
+              (expand-file-name (concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+            (normal-top-level-add-subdirs-to-load-path))))))
+
+(add-to-load-path "elisp" "conf" "public_repos")
+
